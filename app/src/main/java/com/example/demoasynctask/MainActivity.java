be73812d45a011e5ac13760e1f1a5e0deffe1805
Thanh <thanhlv.yb@gmail.com>
2019-02-19 09:59:16 +0700

    main function

--- app/src/main/java/com/example/demoasynctask/MainActivity.java
+++ app/src/main/java/com/example/demoasynctask/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.demoasynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mtxtShow;
+    private Button mbtnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtxtShow = (TextView) findViewById(R.id.txt_show);
+        mbtnStart = (Button) findViewById(R.id.btn_start);
+
+        mbtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask myTask = new MyTask(MainActivity.this, mtxtShow, mbtnStart);
+                myTask.execute();
+                mbtnStart.setEnabled(false);
+            }
+        });
     }
 }
